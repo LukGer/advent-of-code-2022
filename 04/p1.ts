f42e146ae1e6696d4b42e98f28bb873e29119a8d
Lukas Gerhold <lukger1999@gmail.com>
2022-12-04 14:50:02 +0100

    04-cleaning

---
+++ 04/p1.ts
@@ -0,0 +1,23 @@
+const input: string = await Deno.readTextFile("./input.txt");
+
+const lines = input.split("\r\n");
+
+let count = 0;
+
+for(const line of lines){
+    const group1 = line.split(",")[0];
+    const lowerBound1 = Number(group1.split("-")[0]);
+    const upperBound1 = Number(group1.split("-")[1]);
+
+    const group2 = line.split(",")[1];
+    const lowerBound2 = Number(group2.split("-")[0]);
+    const upperBound2 = Number(group2.split("-")[1]);
+
+
+    if((lowerBound1 >= lowerBound2 && upperBound1 <= upperBound2) || (lowerBound2 >= lowerBound1 && upperBound2 <= upperBound1)){
+        count++;
+        continue;
+    }
+}
+
+console.log(count);

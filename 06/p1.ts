487a07e9d7ce6ed93dee3ca9dc3765b08efa22cb
Lukas Gerhold <lukger1999@gmail.com>
2022-12-06 18:38:44 +0100

    06-messages

---
+++ 06/p1.ts
@@ -0,0 +1,19 @@
+const input = await Deno.readTextFile("./input.txt");
+
+for (let i = 0; i < input.length; i++) {
+  const signal = input.substring(i, i + 4);
+
+  let found = true;
+  for (let y = 0; y < 3; y++) {
+    const char = signal[y];
+    if (signal.substring(y + 1).includes(char)) {
+      found = false;
+      break;
+    }
+  }
+
+  if (found) {
+    console.log(signal, i + 4);
+    break;
+  }
+}
